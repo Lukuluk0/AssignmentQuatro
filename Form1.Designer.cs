80e5e22153d6e208c0bad4f81f1487902f3e7a7f
Lukuluk0 <saccoman.lu@gmail.com>
2021-10-26 16:12:34 +0200

    rangement

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,21 +29,183 @@ namespace BlackJackk
         /// </summary>
         private void InitializeComponent()
         {
+            this.pic_croupier1 = new System.Windows.Forms.PictureBox();
+            this.pic_croupier2 = new System.Windows.Forms.PictureBox();
+            this.pic_croupier3 = new System.Windows.Forms.PictureBox();
+            this.pic_croupier4 = new System.Windows.Forms.PictureBox();
+            this.pic_player1 = new System.Windows.Forms.PictureBox();
+            this.pic_player4 = new System.Windows.Forms.PictureBox();
+            this.pic_player3 = new System.Windows.Forms.PictureBox();
+            this.pic_player2 = new System.Windows.Forms.PictureBox();
+            this.line = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_croupier1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_croupier2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_croupier3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_croupier4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_player4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_player3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_player2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pic_croupier1
+            // 
+            this.pic_croupier1.Location = new System.Drawing.Point(26, 294);
+            this.pic_croupier1.Name = "pic_croupier1";
+            this.pic_croupier1.Size = new System.Drawing.Size(125, 176);
+            this.pic_croupier1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_croupier1.TabIndex = 0;
+            this.pic_croupier1.TabStop = false;
+            // 
+            // pic_croupier2
+            // 
+            this.pic_croupier2.Location = new System.Drawing.Point(157, 294);
+            this.pic_croupier2.Name = "pic_croupier2";
+            this.pic_croupier2.Size = new System.Drawing.Size(125, 176);
+            this.pic_croupier2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_croupier2.TabIndex = 1;
+            this.pic_croupier2.TabStop = false;
+            // 
+            // pic_croupier3
+            // 
+            this.pic_croupier3.Cursor = System.Windows.Forms.Cursors.PanSE;
+            this.pic_croupier3.Location = new System.Drawing.Point(288, 294);
+            this.pic_croupier3.Name = "pic_croupier3";
+            this.pic_croupier3.Size = new System.Drawing.Size(125, 176);
+            this.pic_croupier3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_croupier3.TabIndex = 2;
+            this.pic_croupier3.TabStop = false;
+            // 
+            // pic_croupier4
+            // 
+            this.pic_croupier4.Location = new System.Drawing.Point(419, 294);
+            this.pic_croupier4.Name = "pic_croupier4";
+            this.pic_croupier4.Size = new System.Drawing.Size(125, 176);
+            this.pic_croupier4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_croupier4.TabIndex = 3;
+            this.pic_croupier4.TabStop = false;
+            // 
+            // pic_player1
+            // 
+            this.pic_player1.Location = new System.Drawing.Point(654, 294);
+            this.pic_player1.Name = "pic_player1";
+            this.pic_player1.Size = new System.Drawing.Size(125, 176);
+            this.pic_player1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_player1.TabIndex = 4;
+            this.pic_player1.TabStop = false;
+            // 
+            // pic_player4
+            // 
+            this.pic_player4.Location = new System.Drawing.Point(1047, 294);
+            this.pic_player4.Name = "pic_player4";
+            this.pic_player4.Size = new System.Drawing.Size(125, 176);
+            this.pic_player4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_player4.TabIndex = 5;
+            this.pic_player4.TabStop = false;
+            // 
+            // pic_player3
+            // 
+            this.pic_player3.Location = new System.Drawing.Point(916, 294);
+            this.pic_player3.Name = "pic_player3";
+            this.pic_player3.Size = new System.Drawing.Size(125, 176);
+            this.pic_player3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_player3.TabIndex = 6;
+            this.pic_player3.TabStop = false;
+            // 
+            // pic_player2
+            // 
+            this.pic_player2.Location = new System.Drawing.Point(785, 294);
+            this.pic_player2.Name = "pic_player2";
+            this.pic_player2.Size = new System.Drawing.Size(125, 176);
+            this.pic_player2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_player2.TabIndex = 7;
+            this.pic_player2.TabStop = false;
+            // 
+            // line
+            // 
+            this.line.Location = new System.Drawing.Point(598, 25);
+            this.line.Name = "line";
+            this.line.Size = new System.Drawing.Size(10, 489);
+            this.line.TabIndex = 8;
+            this.line.Text = "|||||||||||||||||||||||||";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1209, 28);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem});
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(54, 24);
+            this.startToolStripMenuItem.Text = "Start";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.newToolStripMenuItem.Text = "New";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1209, 523);
+            this.Controls.Add(this.line);
+            this.Controls.Add(this.pic_player2);
+            this.Controls.Add(this.pic_player3);
+            this.Controls.Add(this.pic_player4);
+            this.Controls.Add(this.pic_player1);
+            this.Controls.Add(this.pic_croupier4);
+            this.Controls.Add(this.pic_croupier3);
+            this.Controls.Add(this.pic_croupier2);
+            this.Controls.Add(this.pic_croupier1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_croupier1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_croupier2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_croupier3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_croupier4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_player4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_player3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_player2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pic_croupier1;
+        private System.Windows.Forms.PictureBox pic_croupier2;
+        private System.Windows.Forms.PictureBox pic_croupier3;
+        private System.Windows.Forms.PictureBox pic_croupier4;
+        private System.Windows.Forms.PictureBox pic_player1;
+        private System.Windows.Forms.PictureBox pic_player4;
+        private System.Windows.Forms.PictureBox pic_player3;
+        private System.Windows.Forms.PictureBox pic_player2;
+        private System.Windows.Forms.Label line;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
     }
 }
 
---
+++ Form2.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace BlackJackk
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_nbr_player = new System.Windows.Forms.TextBox();
+            this.txt_nbr_deck = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_nbr_player
+            // 
+            this.txt_nbr_player.Location = new System.Drawing.Point(58, 103);
+            this.txt_nbr_player.Name = "txt_nbr_player";
+            this.txt_nbr_player.Size = new System.Drawing.Size(206, 27);
+            this.txt_nbr_player.TabIndex = 1;
+            // 
+            // txt_nbr_deck
+            // 
+            this.txt_nbr_deck.Location = new System.Drawing.Point(58, 48);
+            this.txt_nbr_deck.Name = "txt_nbr_deck";
+            this.txt_nbr_deck.Size = new System.Drawing.Size(206, 27);
+            this.txt_nbr_deck.TabIndex = 2;
+            this.txt_nbr_deck.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 152);
+            this.Controls.Add(this.txt_nbr_deck);
+            this.Controls.Add(this.txt_nbr_player);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_nbr_player;
+        private System.Windows.Forms.TextBox txt_nbr_deck;
+    }
+}
