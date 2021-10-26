e290e0829c4c5c6c8e6f6090c7962e88a2951950
Lukuluk0 <saccoman.lu@gmail.com>
2021-10-26 15:56:15 +0200

    lezgoo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace BlackJackk
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1201, 963);
+            this.ClientSize = new System.Drawing.Size(1209, 523);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
